e9935f7e6d71594abf78fc50140a7d815855ffb8
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2024-06-25 12:08:21 -0300

    feat(record): record var stuff

--- lib/record.c
+++ lib/record.c
@@ -13,11 +13,38 @@ void freeRecord(record *r)
       free(r->type);
     if (r->opt1 != NULL)
       free(r->opt1);
+
+    for (int i = 0; i < MAX_VARS_RECORD; ++i)
+    {
+      free(r->vars[i]);
+    }
+
+    r->num_vars_used = 0;
     free(r);
   }
 }
 
-record *createRecord(char *c1, char *c2, char *c3)
+void push_var(record *r, record_var *var)
+{
+  if (r->num_vars_used == MAX_VARS_RECORD)
+  {
+    printf("You have used all vars! Sorry. Closing application BOOM...\n");
+    exit(0);
+  }
+
+  r->vars[r->num_vars_used] = var;
+  r->num_vars_used = r->num_vars_used + 1;
+}
+
+void dup_vars(record * r, int i_num_vars, record_var ** i_vars) {
+  for (int i = 0; i < i_num_vars; ++i) {
+    record_var * i_var = i_vars[i];
+    record_var * cp_var = createVar(i_var->name, i_var->kind_of_use, i_var->initial_value, i_var->type);
+    push_var(r, cp_var);
+  }
+}
+
+record *createRecord(record_var ** i_vars, int i_num_vars, char *c1, char *c2, char *c3)
 {
   record *r = (record *)malloc(sizeof(record));
 
@@ -30,6 +57,25 @@ record *createRecord(char *c1, char *c2, char *c3)
   r->code = strdup(c1);
   r->type = strdup(c2);
   r->opt1 = strdup(c3);
-
+  r->num_vars_used = 0;
+  r->vars = (record_var **)malloc(MAX_VARS_RECORD * sizeof(record_var));
+  dup_vars(r, i_num_vars, i_vars);
   return r;
+}
+
+record_var *createVar(char *name, int kind_of_use, char *initial_value, char *type)
+{
+  record_var *v = (record_var *)malloc(sizeof(record_var));
+
+  if (!v)
+  {
+    printf("Allocation problem. Closing application...\n");
+    exit(0);
+  }
+
+  v->name = strdup(name);
+  v->kind_of_use = kind_of_use;
+  v->initial_value = strdup(initial_value);
+  v->type = strdup(type);
+  return v;
 }
--- lib/record.h
+++ lib/record.h
@@ -1,16 +1,33 @@
 #ifndef RECORD
 #define RECORD
 
+#define MAX_VARS_RECORD 331
+
+struct record_var
+{
+	char * name;
+	int kind_of_use; // 0 - declaration 1 - using
+	char * initial_value;
+	char * type;
+};
+
+typedef struct record_var record_var;
+
 struct record
 {
 	char *code; /* field for storing the output code */
 	char *type; /* field for storing the type */
 	char *opt1; /* field for storing the ids */
+	int num_vars_used;
+	record_var ** vars;
 };
 
 typedef struct record record;
 
+void push_var(record *, record_var *);
 void freeRecord(record *);
-record *createRecord(char *, char *, char *);
+void dup_vars(record *, int, record_var **);
+record *createRecord(record_var **, int, char *, char *, char *);
+record_var *createVar(char *, int, char *, char *);
 
 #endif
